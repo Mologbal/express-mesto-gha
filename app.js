c797a99c7bd7acf4c4397b0bb0bff2a8774826f1
Ruslan Kushukov <ruslanoid91@gmail.com>
2022-09-26 15:15:20 +0300

    1st check myfixes

--- app.js
+++ app.js
@@ -14,6 +14,12 @@ const auth = require('./middlewares/auth');
 const { PORT = 3000 } = process.env;
 const app = express();
 
+mongoose.connect('mongodb://127.0.0.1:27017/mestodb', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+app.use(bodyParser.json());
+
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.post('/signup', celebrate({
