4ce7e52991395495563cb7237d1a9d2bbfec8a1e
Ruslan Kushukov <ruslanoid91@gmail.com>
2022-09-26 18:30:00 +0300

    fixed some

--- models/user.js
+++ models/user.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
-const bcrypt = require('bcryptjs');
 const validator = require('validator');
+const bcrypt = require('bcryptjs');
 const BadRequestError = require('../errors/badRequestError');
 const UnauthorizedError = require('../errors/unauthorizedError');
 
@@ -10,25 +10,25 @@ const userSchema = new mongoose.Schema({
     required: false,
     minlength: 2,
     maxlength: 30,
-    default: 'Жак-Ив Кусто',
+    default: "Жак-Ив Кусто",
   },
   about: {
     type: String,
     required: false,
     minlength: 2,
     maxlength: 30,
-    default: 'Исследователь',
+    default: "Исследователь",
   },
   avatar: {
     type: String,
     required: false,
+    default: "https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png",
     validate: {
       validator(v) {
         return /https?:\/\/(w{3}\.)?([\w-]{1,}\.)+[\w._~:/?#[\]@!$&'()*+,;=]*#?/i.test(v);
       },
-      message: 'Передана некорректная ссылка на аватар',
+      message: 'Ссылка на ваш аватар некорректна',
     },
-    default: 'https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png',
   },
   email: {
     type: String,
@@ -36,26 +36,27 @@ const userSchema = new mongoose.Schema({
     unique: true,
     validate(value) {
       if (!validator.isEmail(value)) {
-        throw new BadRequestError({ message: 'Не верный формат электронной почты' });
+        throw new BadRequestError({ message: 'Некорректный вид эл. почты' });
       }
     },
   },
   password: {
     type: String,
     required: true,
-    select: false,
     minlength: 8,
+    select: false,
   },
 });
 
 userSchema.statics.findUserByCredentials = function findOne(email, password) {
   return this.findOne({ email }).select('+password').then((user) => {
     if (!user) {
-      throw new UnauthorizedError('Неправильные почта или пароль');
+      throw new UnauthorizedError('Почта или пароль неверны');
     }
+    //сравним хеш пароля
     return bcrypt.compare(password, user.password).then((matched) => {
       if (!matched) {
-        throw new UnauthorizedError('Неправильные почта или пароль');
+        throw new UnauthorizedError('Почта или пароль неверны');
       }
       return user;
     });
